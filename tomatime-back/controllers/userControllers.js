aa8079fe0640175b9e6647c77373c525f91c362a
Mirko Cirillo <cirillomirko18@gmail.com>
2024-01-31 14:01:01 +0100

    User Login

--- tomatime-back/controllers/userControllers.js
+++ tomatime-back/controllers/userControllers.js
@@ -1,14 +1,67 @@
 const databasePool = require("../db.js");
 
 
-exports.registration = async (req, res) => {
+
+exports.getUserList = async (req, res) => {
+    try{
+        
+        const [userList] = await databasePool
+            .query(`SELECT * FROM User`);
+        return res.status(200).json({
+            msg: "Lista utenti",
+            userList
+        })
+
+    }catch (err) {
+        console.error(err);
+        return res.sendStatus(500);
+    }
+}
+
+exports.login = async (req, res) => {
     try{
         const {username, password} = req.body;
         const [user] = await databasePool
-            .execute(`INSERT INTO User(username, password, date_of_registration) VALUES (?, ?, NOW())`, [username, password]);
-        return res.status(200).json({
-            msg: "Benvenuto"
-        });
+            .query(`
+                SELECT * 
+                FROM User 
+                WHERE username = ?
+                AND password = ?
+            `, [username, password]);
+        if(user.length > 0){
+            return res.status(200).json({
+                msg: `Bentornato ${user[0].username}`,
+                user
+            })
+        }
+        return res.status(400).json({
+            msg: "Nome utente o password errati!",
+            user
+        })
+
+    }catch (err) {
+        console.error(err);
+        return res.sendStatus(500);
+    }
+}
+
+
+exports.registration = async (req, res) => {
+    try{
+        const {username, password} = req.body;
+        if(username.length < 16 && password.length < 8){
+            const [user] = await databasePool
+                .execute(`INSERT INTO User(username, password, date_of_registration) VALUES (?, ?, NOW())`, [username, password]);
+            return res.status(200).json({
+                msg: "Benvenuto"
+            });
+        } if(username.length > 16){
+            return res.status(400).json({
+                msg: "Username troppo lungo per la registrazione"
+            });
+        } return res.status(400).json({
+            msg: "Password troppo lunga"
+        })
     } catch (err){
         console.error(err);
         return res.sendStatus(500);
--- tomatime-back/routes/userRoutes.js
+++ tomatime-back/routes/userRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require("../controllers/userControllers.js");
 
 
+router.get('/', controller.getUserList);
+router.get('/login', controller.login);
 router.post('/register', controller.registration);
 
 
