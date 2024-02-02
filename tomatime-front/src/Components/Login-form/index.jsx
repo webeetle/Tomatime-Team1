95f71996308886df01c861e8ad3231401c9e083e
Mirko Cirillo <cirillomirko18@gmail.com>
2024-02-02 17:13:02 +0100

    Front?

---
+++ tomatime-front/src/Components/Login-form/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import {Link} from "react-router-dom";
+
+function Login(props){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    return (
+        <>
+        </>
+    )
+}
+
+export default Login
