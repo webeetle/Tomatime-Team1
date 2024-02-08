9496965276c5ce5526fb946dabc729154863c9b8
C1rK0x <149179421+C1rK0x@users.noreply.github.com>
2024-02-08 16:01:37 +0100

    Update taskControllers.js
    fix perchè sono scemo

--- tomatime-back/controllers/taskControllers.js
+++ tomatime-back/controllers/taskControllers.js
@@ -8,11 +8,11 @@ exports.getTask = async (req, res) => {
     const {id} = req.body;
     if(id){
         try{
-            const [user] = await databasePool.query("SELECT * FROM Task Where user_id = ?", [id]);
+            const [task] = await databasePool.query("SELECT * FROM Task Where user_id = ?", [id]);
 
-            const todo = [user.filter((element) => element.state == "TODO")];
-            const inProgress = [user.filter((element) => element.state == "DOING")];
-            const done = [user.filter((element) => element.state == "DONE")];
+            const todo = [task.filter((element) => element.state == "TODO")];
+            const inProgress = [task.filter((element) => element.state == "DOING")];
+            const done = [task.filter((element) => element.state == "DONE")];
 
             return res.status(200).json({
                 msg: "TASK: ",
